b978f61b86365b77085bd849da039c6a6d2c4085
Johnny <1136798369@qq.com>
2018-10-12 12:34:22 +0800

    selectionSort

---
+++ Sort/Sort/SelectionSort.h
@@ -0,0 +1,18 @@
+//
+//  SelectionSort.h
+//  Sort
+//
+//  Created by JianingWang on 10/12/18.
+//  Copyright © 2018 王嘉宁. All rights reserved.
+//
+
+#ifndef SelectionSort_h
+#define SelectionSort_h
+
+//template<typename T>
+class SelectionSort {
+public:
+    SelectionSort();
+};
+
+#endif /* SelectionSort_h */
--- Sort/Sort/main.cpp
+++ Sort/Sort/main.cpp
@@ -7,9 +7,30 @@
 //
 
 #include <iostream>
+#include <algorithm>
+using namespace std;
+
+template<typename T>
+T* selectionSort(T* array, int length);
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int arr[10] = {4,6,1,32,3,6,2,24,5,1};
+    selectionSort(arr, 10);
+    for (int i = 0; i < 10; i++) {
+        cout<<arr[i]<<" ";
+    }
     return 0;
+}
+
+template<typename T>
+void selectionSort(T* array, int length){
+    for (int i = 0; i < length; i++) {
+        int min = i;
+        for (int j = i+1; j < length; j++) {
+            if (array[j] < array[min]) {
+                min = j;
+            }
+            swap(array[i], array[min]);
+        }
+    }
 }
